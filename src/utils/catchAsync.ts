662d6688f7eae7d2036058513b07d398b41b7be6
Rashed <tools.rashed@gmail.com>
2025-01-18 14:06:21 +0600

    Fixed console errors

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -7,25 +7,3 @@ const catchAsync = (func: RequestHandler) => {
 }
 
 export default catchAsync
-
-// catchAsync(async (req, res) => {
-
-//     const result = await userService.getUser()
-
-//     sendResponse(res, {
-//       statusCode: StatusCodes.OK,
-//       message: 'Users getting successfully',
-//       data: result,
-//     })
-
-// })
-
-// function createOperation(func: (a: number, b: number) => number) {
-//   return func
-// }
-
-// const add = createOperation((a, b) => a + b)
-// const multiply = createOperation((a, b) => a * b)
-
-// console.log(add(3, 5))
-// console.log(multiply(3, 5))
